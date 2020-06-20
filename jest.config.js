1fcd2f6fd15282185d1cd9fc735f1e8bae120a1f
jihchi <achi@987.tw>
2020-06-20 08:53:43 +0800

    Adopt bisect_ppx library

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['bisect_ppx/src/runtime/bucklescript/jest.js'],
   testPathIgnorePatterns: ['/node_modules/', '<rootDir>/__tests__/support/'],
   coveragePathIgnorePatterns: ['/node_modules/', '<rootDir>/__tests__/'],
 };
